0e7b6855119e26121e1d512447ef355a4ec221c4
binnama <laasestad@yahoo.no>
2024-08-15 14:45:50 +0200

    Debug practice

---
+++ LearnPython/Testing/Debugging/mainPractice.py
@@ -0,0 +1,7 @@
+def unlock_achievement(before_xp, ach_xp, ach_name):
+
+    after_xp = before_xp + ach_xp
+
+    achievementUnlocked = (f"Achievement Unlocked: " + ach_name)
+
+    return after_xp, achievementUnlocked
