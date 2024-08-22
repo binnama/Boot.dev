c0b11bd83ecce3de3bf871cf5327ae8fec683eea
binnama <laasestad@yahoo.no>
2024-08-22 14:46:13 +0200

    Bits and converter

---
+++ LearnPython/Computing/ConvertBin/main.py
@@ -0,0 +1,7 @@
+def binary_string_to_int(num_servers, num_players, num_admins):
+    server_num = int(num_servers, 2)
+    player_num = int(num_players, 2)
+    admin_num = int(num_admins, 2)
+
+    return server_num, player_num, admin_num
+
