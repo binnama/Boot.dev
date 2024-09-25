2cec80065cec9394cfc46df268cf91f81c9d29c0
binnama <laasestad@yahoo.no>
2024-09-25 15:25:55 +0200

    Rest of Dictionaries

--- LearnPython/Dictionaries/DictTotScore/main.py
+++ LearnPython/Dictionaries/DictTotScore/main.py
@@ -1,5 +1,17 @@
 def merge(dict1, dict2):
-    pass
+    #print(f"Dict 1: {dict1}")
+    #print(f"Dict 2: {dict2}")
+    merged_dicts = dict1.copy()
+    merged_dicts.update(dict2)
+
+    #print(f"Merged Dicts: {merged_dicts}")
+    return merged_dicts
 
 def total_score(score_dict):
-    pass
+    sum_score = 0
+    temp_score = score_dict.values()
+    sum_score = sum(temp_score)
+
+    return sum_score
+
+
